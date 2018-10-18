9054ab2233ac0a92c545427904eae3b602a849d0
Kristian Angelski <kristian.angelski@gmail.com>
2018-10-18 13:31:05 +0300

    adding puppy-age solution

---
+++ functions/solution/puppy-age.js
@@ -0,0 +1,9 @@
+function calculateDogAge(age) {
+  var humanYears = age * 7;
+  
+  console.log('Your doggie is ' + humanYears + ' years old in dog years!');
+}
+
+calculateDogAge(3);
+calculateDogAge(5);
+calculateDogAge(7);
